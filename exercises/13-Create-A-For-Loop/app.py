7fb45821d816498d74cf266118f13b759667c3ed
ricardoyepezz <ricardoyepezz@gmail.com>
2022-04-12 00:45:34 +0000

    exercise 15 done

--- exercises/13-Create-A-For-Loop/app.py
+++ exercises/13-Create-A-For-Loop/app.py
@@ -1,4 +1,7 @@
 def standards_maker():
-    #your code here
+    for x in range(300):
+        print("I will write questions if I am stuck") 
+
+standards_maker()
 
 #remember to call the function outside (here)
